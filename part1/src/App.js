4102b6a33552fecfcd8ceea06c1edc1b5176f101
Ramon sorto <ramonsorto1@gmail.com>
2021-04-06 12:49:17 -0600

    bootcamp part1

---
+++ part1/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">  
+      <h1>bootcamp</h1>
+      <strong>estamos iniciando</strong>  
+   
+    </div>
+  );
+}
+
+export default App;
